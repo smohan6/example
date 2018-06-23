fdd48814bceaca942ad1a021fa60caf9ba7516fd
SFA17SCM13M <sanjaym@securetech360.com>
2018-06-23 16:05:45 -0700

    My initial commit message

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
-git branch registration
+git checkout registration
 git add *
 git commit -m "My initial commit message"
-git push -u origin development
+git push -u origin registration
 git merge master
 git checkout master 
-git merge development
+git merge registration
+git push -u origin master
