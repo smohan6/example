6b88c1ec8b8c895dd9f7e56876f6576097dbff0b
smohan6 <smohan6@hawk.iit.edu>
2018-06-24 22:05:41 -0700

    Committeddd

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
+git checkout registration
 git add *
 git commit -m "My initial commit message"
 git push -u origin registration
 git merge master
 git checkout master 
-git merge registration
+git merge development
 git push -u origin master
